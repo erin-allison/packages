08a14a4a21cf2acbd52306710bd482b0dc11d7ea
Erin Allison <erin@eallison.us>
2021-11-30 12:09:22 -0600

    Use rootless podman
    Signed-off-by: Erin Allison <erin@eallison.us>

--- build.sh
+++ build.sh
@@ -6,11 +6,8 @@ dir=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
 
 mkdir -p pkg/
 
-cmd="cd '$dir'; "
-cmd+='id=`podman build -q . --no-cache`; '
-cmd+='podman run -it --rm -v $(pwd)/pkg/:/home/build/ea-private/ $id bash /home/build/packages/in-container.sh; '
-
-echo $cmd | pkexec /bin/bash
+podman build . --no-cache --iidfile image-id
+podman run -it --rm -v $(pwd)/pkg/:/home/build/ea-private/ $(cat image-id) bash /home/build/packages/in-container.sh
 
 ln -sf ea-private.db.tar.xz pkg/ea-private.db
 ln -sf ea-private.files.tar.xz pkg/ea-private.files
